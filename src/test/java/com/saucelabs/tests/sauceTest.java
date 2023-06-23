d126fd028ef898d31a8ae435c08659a39cb6c621
wirelisa <isa.arslan@hotmail.com>
2023-06-23 17:48:36 +0200

    first commit

---
+++ src/test/java/com/saucelabs/tests/sauceTest.java
@@ -0,0 +1,64 @@
+package com.saucelabs.tests;
+
+import com.saucelabs.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class sauceTest {
+    protected WebDriver driver;
+    protected Actions action;
+    protected WebDriverWait wait;
+    protected String url;
+
+    protected RemoteWebDriver remoteDriver;
+
+    @BeforeMethod
+    public void setup(Method method) throws MalformedURLException {
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", "oauth-wirelisa35-46071");
+        sauceOptions.setCapability("accessKey", "c0e185e9-8f85-464c-abbf-c8e941fb89a3");
+        sauceOptions.setCapability("name", method.getName());
+        sauceOptions.setCapability("browserVersion", "latest");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com/wd/hub");
+
+        remoteDriver = new RemoteWebDriver(url, options);
+    }
+
+
+    @Test
+    public void runOnCloud(){
+        driver = remoteDriver;
+        driver.get("https://www.saucedemo.com");
+        // Inputs standard_user in username field.
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        // Inputs secret_sauce in password field
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+
+        // Clicks the Login button
+        driver.findElement(By.id("login-button")).click();
+
+        // maximizes browser window
+        driver.manage().window().maximize();
+
+        // Closes browser
+        driver.quit();
+
+
+    }
+
+
+}
---
+++ src/test/java/com/saucelabs/utilities/Driver.java
@@ -0,0 +1,91 @@
+package com.saucelabs.utilities;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.net.URL;
+import java.util.Arrays;
+
+public class Driver {
+    private Driver() {
+
+    }
+
+    private static WebDriver driver;
+    private static RemoteWebDriver remoteDriver;
+
+    public static WebDriver get() {
+
+        if (driver == null) {
+            String browser = ConfigurationReader.get("browser");
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking","enable-automation"));
+                    options.addArguments("disable-notifications");
+                    options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                    options.addArguments( "--disable-blink-features=AutomationControlled");
+                    options.addArguments("--remote-allow-origins=*");
+                    driver = new ChromeDriver(options);
+
+
+
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "firefox-headless":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
+                    break;
+                case "ie":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Internet Explorer");
+                    WebDriverManager.iedriver().setup();
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                case "edge":
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+
+                case "safari":
+                    if (!System.getProperty("os.name").toLowerCase().contains("mac"))
+                        throw new WebDriverException("Your OS doesn't support Safari");
+                    WebDriverManager.getInstance(SafariDriver.class).setup();
+                    driver = new SafariDriver();
+                    break;
+            }
+
+
+
+        }
+
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
